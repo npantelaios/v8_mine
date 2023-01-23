f202d864de48902003c82166342b8b2785c78849
npantelaios <npantel@ncsu.edu>
2023-01-23 11:06:11 -0500

    testcase modified, print message added

--- src/out/test.js
+++ src/out/test.js
@@ -1,5 +1,9 @@
 // How to execute this with d8:
 // out/x64.debug/d8 test.js
+//
+// print bytecode:
+// x64.debug/d8 --print-bytecode test.js
+
 console.log("Hello world!");
 
 if (1) {
@@ -7,3 +11,9 @@ if (1) {
 } else {
   console.log("Else statement");
 }
+
+function incrementX(obj) {
+  return 1 + obj.x;
+}
+incrementX({ x: 42 });
+c;
