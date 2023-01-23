a70e731221fda33d23a510b229a021072b543505
npantelaios <npantel@ncsu.edu>
2023-01-23 10:29:35 -0500

    testcase changed

--- src/out/test.js
+++ src/out/test.js
@@ -1,3 +1,9 @@
 // How to execute this with d8:
 // out/x64.debug/d8 test.js
 console.log("Hello world!");
+
+if (1) {
+  console.log("Hello world from inside if");
+} else {
+  console.log("Else statement");
+}
